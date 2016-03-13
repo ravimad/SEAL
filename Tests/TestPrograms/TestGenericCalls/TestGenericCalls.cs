2d140a4c523fb70244b94499f441691760c916ff
Ravi <ravi.kandhadai@epfl.ch>
2016-03-14 00:10:39 +0100

    Initializing repo

---
+++ Tests/TestPrograms/TestGenericCalls/TestGenericCalls.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGenericCalls
+{
+    public class TestGenericCalls
+    {
+        public TestGenericCalls f;
+        public int g;        
+
+        public override int GetHashCode()
+        {
+            f.g = 10;
+            return base.GetHashCode();
+        }          
+
+        public void bar<T>(T arg1)
+        {
+            arg1.GetHashCode();
+        }
+
+        public void foo(TestGenericCalls inp)
+        {
+            TestGenericCalls ins = new TestGenericCalls();
+            ins.f = inp;
+            bar(ins);
+        }
+
+        public struct TStruct
+        {
+            public TestGenericCalls f;
+            int g;
+            public override int GetHashCode()
+            {                
+                return f.GetHashCode();
+            }
+        }
+        public void StructFun(TestGenericCalls inp)
+        {
+            TStruct ins = new TStruct();
+            ins.f = inp;
+            bar(ins); // here, the struct is copied, which means the write happen to local object
+        }
+    }
+}
---
+++ Tests/TestPrograms/TestReferenceParams/TestReferenceParams.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestReferenceParams
+{
+    public class TestReferenceParams
+    {
+        public TestReferenceParams f;
+        public int g;
+        virtual public void mutatef()
+        {
+            f.g = 10;
+        }
+
+        public void bar(ref TestReferenceParams ins)
+        {
+            ins.mutatef();
+        }
+
+        public void foo(TestReferenceParams inp)
+        {
+            TestReferenceParams ins = new TestReferenceParams();
+            ins.f = inp;
+            bar(ref ins);
+        }
+
+        public struct TStruct
+        {
+            public TestReferenceParams f;          
+
+            public void mutateCaller(ref TStruct s)
+            {
+                s.f.mutatef();
+            }
+
+            public void StructFun(TestReferenceParams inp)
+            {
+                TStruct ins = new TStruct();
+                ins.f = inp;
+                mutateCaller(ref ins); 
+            }
+        }
+       
+    }
+}
